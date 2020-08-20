acd121c95a0ce6521fcf2631f366bab3871210b8
xhanglog <blogxiaohang@163.com>
2020-08-20 21:58:52 +0800

    学习stream流

---
+++ learn-codesource/src/main/java/xhang/learnStream/LearnStream.java
@@ -0,0 +1,36 @@
+package xhang.learnStream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author xhang
+ * 请按照给出数据，找出同时满足偶数ID且年龄大于24且用户名转为大写且用户名字母倒排序最后只输出一个用户名字。
+ **/
+
+public class LearnStream {
+
+    public static void main(String[] args) {
+
+        User u1 = new User(11,"a",23);
+        User u2 = new User(12,"b",24);
+        User u3 = new User(13,"c",22);
+        User u4 = new User(14,"d",28);
+        User u5 = new User(16,"e",26);
+
+        List<User> list = Arrays.asList(u1,u2,u3,u4,u5);
+
+        /**
+         * filter 里边是filter(Predicate<? super T> predicate) 判定型接口
+         *
+         * map map(Function<? super T, ? extends R> mapper) 函数型接口
+         */
+        list.stream().filter(p->{return p.getId() %2 == 0;}) // 筛选偶数ID
+                .filter(p->{return p.getAge() > 24;}) //筛选年龄大于24
+                .map(u->{return u.getUserName().toUpperCase();})
+                .forEach(System.out::println);
+    }
+}
+
+
+
---
+++ learn-codesource/src/main/java/xhang/learnStream/User.java
@@ -0,0 +1,53 @@
+package xhang.learnStream;
+
+/**
+ * @Author xhang
+ **/
+public class User {
+
+    private Integer id;
+    private String  userName;
+    private int age;
+
+    public User() {
+    }
+
+    public User(Integer id, String userName, int age) {
+        this.id = id;
+        this.userName = userName;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
